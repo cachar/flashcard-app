22715359e2fb9afef6f8617865cabd3277b5811c
Hackbright Student <info@hackbrightacademy.com>
2016-02-09 17:42:17 -0800

    started working on making cards

--- server.py
+++ server.py
@@ -25,10 +25,24 @@ def show_card_decks():
 def create_card_deck():
     
     card_deck= CardDeck(field=Politician.questionable_field())
+    politician = Politician.query.get(1)
+    card = PoliticianCard(card_deck=card_deck, politician=politician)
+    #make many politician_cards
+    db.session.add(card)
+
     db.session.add(card_deck)
     db.session.commit()
 
     return redirect('/card_decks')
+
+@app.route('/card_decks/<int:id>')
+def show_card_deck(id):
+
+    card_deck = CardDeck.query.get(id)
+
+    return render_template("card_deck_details.html", card_deck=card_deck)
+
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
