ce8092844c2c1f361663f9aa92a2a077404e9e2c
Chandrika Achar and Doc Ritezel <chandrika.achar+chandrika+doc@gmail.com>
2016-02-23 17:06:03 -0800

    abstractified seed and put stuff in service

--- seed.py
+++ seed.py
@@ -1,16 +1,12 @@
 """Utility file to seed politician info database from Sunlight Foundation API"""
 
-from sqlalchemy import func
-from model import Politician
+
 
 from model import connect_to_db, db
 from server import app
-
-import requests
-import os
+from service import SunlightClient, PoliticianImporter, ExecutivePresenter
 
 
-SUNLIGHT_API_KEY = os.environ['SUNLIGHT_API_KEY']
 HACKBRIGHT_LATITUDE = "37.788666"
 HACKBRIGHT_LONGITUDE = "-122.411462"
 
@@ -18,136 +14,6 @@ HACKBRIGHT_LONGITUDE = "-122.411462"
 
 
 
-def get_congress_legislators(HACKBRIGHT_LATITUDE=HACKBRIGHT_LATITUDE,
-                             HACKBRIGHT_LONGITUDE=HACKBRIGHT_LONGITUDE,
-                             SUNLIGHT_API_KEY=SUNLIGHT_API_KEY):
-    payload = {"server": "nginx/1.1.19",
-               "date": "Tue, 09 Feb 2016 00:26:43 GMT",
-               "content-type": "application/json; charset=utf-8",
-               "content-length": "2712",
-               "connection": "keep-alive",
-               "vary": "Accept-Encoding",
-               "status": "200 OK"}
-
-    url = "http://congress.api.sunlightfoundation.com/legislators/locate?latitude=%s&longitude=%s&apikey=%s" % (HACKBRIGHT_LATITUDE,
-                                                                                                                HACKBRIGHT_LONGITUDE,
-                                                                                                                SUNLIGHT_API_KEY)
-
-    r = requests.get(url, params=payload)
-    jdict = r.json()
-
-    for result in jdict["results"]:
-        bioguide_id = result['bioguide_id']
-        name = result['first_name'] + " " + result['last_name']
-        party = result['party']
-        title = result['title']
-        if title == "Sen":
-            title = "Senator"
-            constituency = result['state_name']
-        elif title == "Rep":
-            title = "Representative"
-            constituency = result['state_name'] + " district " + str(result['district'])
-        photo_url = 'https://theunitedstates.io/images/congress/225x275/' + bioguide_id + '.jpg'
-
-        politician = Politician.query.filter(Politician.bioguide_id == bioguide_id).first()
-
-        if politician == None:
-            politician = Politician(bioguide_id=bioguide_id,
-                                    name=name,
-                                    title=title,
-                                    constituency=constituency,
-                                    party=party,
-                                    photo_url=photo_url)
-            db.session.add(politician)
-        else:
-            politician.name = name
-            politician.party = party
-            politician.title = title
-            politician.constituency = constituency
-
-    db.session.commit()
-
-
-
-def get_state_reps(HACKBRIGHT_LATITUDE=HACKBRIGHT_LATITUDE,
-                   HACKBRIGHT_LONGITUDE=HACKBRIGHT_LONGITUDE,
-                   SUNLIGHT_API_KEY=SUNLIGHT_API_KEY):
-
-    payload = {"server": "nginx/1.4.1 (Ubuntu)",
-               "content-type": "application/json; charset=utf-8",
-               "vary": "Authorization",
-               "access-control-allow-origin": "*",
-               "content-length": "11097",
-               "date": "Tue, 09 Feb 2016 00:35:05 GMT",
-               "x-varnish": "1674567739",
-               "age": "0",
-               "via": "1.1 varnish",
-               "connection": "keep-alive"}
-
-    url = "http://openstates.org/api/v1//legislators/geo/?lat=%s&long=%s&apikey=%s" % (HACKBRIGHT_LATITUDE,
-                                                                                       HACKBRIGHT_LONGITUDE,
-                                                                                       SUNLIGHT_API_KEY)
-    alt_pic = {}
-    for row in open("static/photos.txt"):
-        row = row.rstrip()
-        alt_bioguide_id, alt_photo_url = row.split(" ")
-        alt_pic[alt_bioguide_id] = alt_photo_url
-    print alt_pic
-
-
-    r = requests.get(url, params=payload)
-    jdict = r.json()
-
-    for result in jdict:
-        name = result["full_name"]
-        bioguide_id = result["leg_id"]
-        party = result["party"][0]
-        photo_url = result["photo_url"]
-        if bioguide_id in alt_pic:
-            photo_url = alt_pic[bioguide_id]
-
-        if result["chamber"] == "lower":
-            title = "Assemblymember"
-        elif result["chamber"] == "upper":
-            title = "Senator"
-        constituency = result["state"].upper() + " state district " + result["district"]
-
-        politician = Politician.query.filter(Politician.bioguide_id == bioguide_id).first()
-
-        if politician == None:
-            politician = Politician(bioguide_id=bioguide_id,
-                                    name=name,
-                                    title=title,
-                                    constituency=constituency,
-                                    party=party,
-                                    photo_url=photo_url)
-            db.session.add(politician)
-        else:
-            politician.name = name
-            politician.party = party
-            politician.title = title
-            politician.constituency = constituency
-
-    db.session.commit()
-
-def hard_code_pres_and_vp():
-    politician = Politician(bioguide_id="POTUS44",
-                            name="Barack Obama",
-                            title="President",
-                            constituency="USA",
-                            party="D",
-                            photo_url="http://a5.files.biography.com/image/upload/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE4MDAzNDEwNzg5ODI4MTEw.jpg")
-    db.session.add(politician)
-
-    politician = Politician(bioguide_id="VPforP44",
-                            name="Joe Biden",
-                            title="Vice President",
-                            constituency="USA",
-                            party="D",
-                            photo_url="https://upload.wikimedia.org/wikipedia/commons/e/ea/Official_portrait_of_Vice_President_Joe_Biden.jpg")
-    db.session.add(politician)
-
-    db.session.commit()
 
 
 
@@ -155,9 +21,11 @@ def load_politicians():
     """Load politicians from Sunlight Foundation API."""
 
     # Get federal congressional representatives.
-    get_congress_legislators()
-    get_state_reps()
-    hard_code_pres_and_vp()
+    client = SunlightClient()
+    PoliticianImporter(client.fetch_congress).add_or_update(HACKBRIGHT_LATITUDE, HACKBRIGHT_LONGITUDE)
+    PoliticianImporter(client.fetch_state_ppl).add_or_update(HACKBRIGHT_LATITUDE, HACKBRIGHT_LONGITUDE)
+    PoliticianImporter(ExecutivePresenter.fetch).add_or_update(HACKBRIGHT_LATITUDE, HACKBRIGHT_LONGITUDE)
+
 
 if __name__ == "__main__":
     connect_to_db(app)
---
+++ service.py
@@ -0,0 +1,192 @@
+
+import requests
+import os
+
+from sqlalchemy import func
+from model import Politician, db
+
+
+class SunlightClient(object):
+
+    def __init__(self):
+        self.key = os.environ['SUNLIGHT_API_KEY']
+
+    def fetch_congress(self, lat, long):
+        url = "http://congress.api.sunlightfoundation.com/legislators/locate"
+        payload = {"latitude": lat,
+                    "longitude": long,
+                    "apikey": self.key}
+
+        r = requests.get(url, params=payload)
+        results = r.json()['results']
+        return [CongressPresenter(result) for result in results]
+
+    def fetch_state_ppl(self, lat, long):
+        url = "http://openstates.org/api/v1//legislators/geo/"
+
+        payload = {"lat": lat,
+                   "long": long,
+                   "apikey": self.key}
+
+        r = requests.get(url, params=payload)
+        results = r.json()
+        return [StatePresenter(result) for result in results]
+
+
+class CongressPresenter(object):
+
+    def __init__(self, result):
+        self.result = result
+
+    def bioguide_id(self):
+        return self.result['bioguide_id']
+
+    def name(self):
+        return self.result['first_name'] + " " + self.result['last_name']
+
+    def title(self):
+        if self.result['title'] == "Sen":
+            return "Senator"
+        elif self.result['title'] == "Rep":
+            return "Representative"
+        else:
+            return self.result['title']
+
+    def constituency(self):
+        if self.result['district'] == None:
+            return self.result['state_name']
+        else:
+            return self.result['state_name'] + " district " + str(self.result['district'])
+
+    def party(self):
+        return self.result['party']
+
+    def photo_url(self):
+        return 'https://theunitedstates.io/images/congress/225x275/' + self.bioguide_id() + '.jpg'
+
+
+
+class StatePresenter(object):
+
+
+
+    def __init__(self, result):
+        self.result = result
+
+    def bioguide_id(self):
+        return self.result['leg_id']
+
+    def name(self):
+        return self.result['full_name']
+
+    def title(self):
+        if self.result["chamber"] == "lower":
+            return "Assemblymember"
+        elif self.result["chamber"] == "upper":
+            return "Senator"
+        else:
+            return "Representative"
+
+    def constituency(self):
+        return self.result["state"].upper() + " state district " + self.result["district"]
+
+    def party(self):
+        return self.result['party'][0]
+
+    def photo_url(self):
+        photo_url = self.result['photo_url']
+
+        return photo_url
+
+
+
+class ExecutivePresenter(object):
+
+    @classmethod
+    def fetch(cls, lat, long):
+        return [cls.president(), cls.vice_president()]
+
+    @classmethod
+    def president(cls):
+        return ExecutivePresenter(bioguide_id="POTUS44",
+                                  name="Barack Obama",
+                                  title="President",
+                                  constituency="USA",
+                                  party="D",
+                                  photo_url="http://a5.files.biography.com/image/upload/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE4MDAzNDEwNzg5ODI4MTEw.jpg")
+
+    @classmethod
+    def vice_president(cls):
+        return ExecutivePresenter(bioguide_id="VPforP44",
+                                  name="Joe Biden",
+                                  title="Vice President",
+                                  constituency="USA",
+                                  party="D",
+                                  photo_url="https://upload.wikimedia.org/wikipedia/commons/e/ea/Official_portrait_of_Vice_President_Joe_Biden.jpg")
+
+    def __init__(self, **dict):
+        self.dict = dict
+
+    def bioguide_id(self):
+        return self.dict['bioguide_id']
+
+    def name(self):
+        return self.dict['name']
+
+    def title(self):
+        return self.dict['title']
+
+    def constituency(self):
+        return self.dict['constituency']
+
+    def party(self):
+        return self.dict['party']
+
+    def photo_url(self):
+        return self.dict['photo_url']
+
+
+
+
+
+class PoliticianImporter(object):
+
+    def __init__(self, fetch):
+
+        self.fetch = fetch
+        self.alt_pic = PoliticianImporter.find_alt_pics()
+
+
+
+    def add_or_update(self, lat, long):
+        people = self.fetch(lat, long)
+        for person in people:
+            politician = Politician.query.filter(Politician.bioguide_id == person.bioguide_id()).first()
+            if politician == None:
+                politician = Politician(
+                    bioguide_id= person.bioguide_id(),
+                    name= person.name(),
+                    title= person.title(),
+                    constituency= person.constituency(),
+                    party= person.party(),
+                    photo_url= self.alt_pic.get(person.bioguide_id(), person.photo_url()),
+                )
+                db.session.add(politician)
+            else:
+                politician.name = person.name()
+                politician.party = person.party()
+                politician.title = person.title()
+                politician.constituency = person.constituency()
+                politician.photo_url = self.alt_pic.get(person.bioguide_id(), person.photo_url())
+
+        db.session.commit()
+
+    @classmethod
+    def find_alt_pics(cls):
+        alt_pic = {}
+        for row in open("static/photos.txt"):
+            row = row.rstrip()
+            alt_bioguide_id, alt_photo_url = row.split(" ")
+            alt_pic[alt_bioguide_id] = alt_photo_url
+
+        return alt_pic
